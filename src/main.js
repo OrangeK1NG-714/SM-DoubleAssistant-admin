811f73f5556a35cc7a22b8ef7783f0d22de85429
18352978082 <1091123779@qq.com>
2026-05-16 19:02:24 +0800

    fix: 手动导入 Element Plus Message/MessageBox 组件样式
    按需导入模式下 ElMessage 和 ElMessageBox 样式丢失，手动补充 CSS 导入

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import Particles from "@tsparticles/vue3";
 import { loadSlim } from "@tsparticles/slim";
 import "@/util/axios.config";
+import "element-plus/es/components/message/style/css";
+import "element-plus/es/components/message-box/style/css";
 
 const app = createApp(App)
 
